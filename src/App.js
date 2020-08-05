42eee9399ac4ef2be31e96a7aa14ad3ad0eb000c
Ruize Nie <rz376775@dal.ca>
2020-08-05 16:49:16 -0300

    customize theme and dynamic load package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+import { message, Button, Space } from "antd";
 
-function App() {
-  return <div>App</div>;
-}
+const success = () => {
+  message.success("This is a success message");
+};
+
+const error = () => {
+  message.error("This is an error message");
+};
+
+const warning = () => {
+  message.warning("This is a warning message");
+};
+
+const App = () => {
+  return (
+    <Space>
+      <Button onClick={success}>Success</Button>
+      <Button onClick={error}>Error</Button>
+      <Button onClick={warning}>Warning</Button>
+    </Space>
+  );
+};
 
 export default App;
